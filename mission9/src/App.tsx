30618eb88b9e1f6310d4d00f5f59bfcb5149afe1
prestonvance <prestonvance@PRESTONVANC1559>
2024-03-09 17:31:38 -0700

    everything's working commit

--- mission9/src/App.tsx
+++ mission9/src/App.tsx
@@ -1,24 +1,58 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
+
+// Assuming the JSON file is structured like this:
+// { "teams": [ ... ] }
+import schoolData from "./CollegeBasketballTeams.json";
+
+interface SchoolProps {
+  tid: number;
+  school: string;
+  name: string;
+  city: string;
+  state: string;
+}
+
+function Welcome() {
+  return (
+    <div>
+      <h1>Welcome to March Madness</h1>
+      <h2>Where Miracles are Made</h2>
+    </div>
+  );
+}
+
+class Team extends React.Component<SchoolProps> {
+  render() {
+    const oneTeam = this.props;
+
+    return (
+      <div>
+        <h3>{oneTeam.school}</h3>
+        <h4>Mascot: {oneTeam.name}</h4>
+        <h4>
+          Location: {oneTeam.city}, {oneTeam.state}
+        </h4>
+      </div>
+    );
+  }
+}
+
+function TeamList() {
+  return (
+    <div>
+      {schoolData.teams.map((oneTeam: SchoolProps, index: number) => (
+        <Team key={index} {...oneTeam} />
+      ))}
+    </div>
+  );
+}
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Welcome />
+      <TeamList />
     </div>
   );
 }
